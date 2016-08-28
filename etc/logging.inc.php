bf27db19dc2d929067e3adbc713b619200b4ac11
matthias <matthias@ich-digital.de>
2016-08-28 20:29:45 +0200

    setup processors

--- etc/logging.inc.php
+++ etc/logging.inc.php
@@ -5,6 +5,11 @@
 # @see https://github.com/Seldaek/monolog/blob/master/doc/01-usage.md#log-levels
 
 /* @var \Monolog\Logger $logger */
+use Monolog\Processor\MemoryPeakUsageProcessor;
+use Monolog\Processor\MemoryUsageProcessor;
+use Monolog\Processor\ProcessIdProcessor;
+use Monolog\Processor\PsrLogMessageProcessor;
+
 /* @var string $logDir */
 /* @var string $logFileName */
 
@@ -12,3 +17,9 @@ $logger->pushHandler(new \Monolog\Handler\StreamHandler(
     "$logDir/$logFileName.debug.log",
     \Monolog\Logger::DEBUG)
 );
+
+
+$logger->pushProcessor(new PsrLogMessageProcessor());
+$logger->pushProcessor(new ProcessIdProcessor());
+$logger->pushProcessor(new MemoryUsageProcessor());
+$logger->pushProcessor(new MemoryPeakUsageProcessor());

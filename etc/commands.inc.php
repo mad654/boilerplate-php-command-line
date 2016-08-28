433a968940d8313abdd8fd9c73a0c473cbbb3aca
matthias <matthias@ich-digital.de>
2016-08-28 19:24:19 +0200

    logging introduced in example

--- etc/commands.inc.php
+++ etc/commands.inc.php
@@ -3,6 +3,9 @@
 // List of active command instances
 // which are subclasses of `\Symfony\Component\Console\Command\Command`
 
+// you can push a logger to your commands constructor
+/* @var \Psr\Log\LoggerInterface $logger */
+
 return [
-    new \Example\Console\HelloWorldCommand()
+    new \Example\Console\HelloWorldCommand($logger)
 ];
---
+++ etc/logging.inc.php
@@ -0,0 +1,6 @@
+<?php
+
+# init monolog handlers
+# @see https://github.com/Seldaek/monolog
+
+/* @var \Monolog\Logger $logger */
